473feefffbb7e8f1871ff11ce945490334b841c9
AngelMB29 <mbucio15@alumnos.uaq.mx>
2020-03-04 17:02:12 -0600

    Buenas Noches

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next)=>{
+    res.send("Bienvenido al servidor");
+});
+
+
+app.get('/:name', (req, res, next)=>{
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola mundo :p " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
+   console.log("Server is running..."); 
+   
+});
